cc2a152022bbe26a88b1f2ec8e6147c7afcea75e
666ggser <112387983+666ggser@users.noreply.github.com>
2022-08-29 18:05:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx7a700ad897650e65",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "f525f8f784065d41f488701887947497",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "hkujhte0-LNoDSrXBWv88r1Pdkava43_zii1xbrr5uQ",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: [""],
      
@@ -19,31 +19,29 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "新疆",
     // 所在城市
-    CITY: "惠州",
+    CITY: "喀什",
 
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
-      {"name": "老婆", "year": "1993", "date": "12-27"},
-      {"name": "家公", "year": "1993", "date": "08-09"},
+      {"name": "小sb", "year": "2005", "date": "11-30"},
+      {"name": "大sb", "year": "2006", "date": "07-30"},
     ],
 
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
-    // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    LOVE_DATE: "2021-08-14",
 
 
     /** 好文节选 */
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: ""
+    LITERARY_PREFERENCE: "平安喜乐"
 
 
     }
@@ -54,7 +52,6 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
